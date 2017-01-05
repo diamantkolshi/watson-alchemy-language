eb125520516a41c9e4bfbc162b9cd8fb0a7e5673
Diamant <diamantkolshi@gmail.com>
2017-01-05 14:54:59 +0100

    add main module

--- lib/alchemy_language.rb
+++ lib/alchemy_language.rb
@@ -2,7 +2,17 @@ Dir.glob(File.join("alchemy_language", "**", "*.rb")).each do |klass|
   require_relative klass
 end
 require 'uri'
-require 'rest-client'
+# require 'rest-client'
 
 module AlchemyLanguage
+  def self.base_url_request
+    "https://gateway-a.watsonplatform.net/calls"
+  end
+
+  def self.secret_token
+    "secret_token"
+  end
 end
+
+# obj = AlchemyLanguage::UrlService.new("www.ibm.com")
+# obj.author
