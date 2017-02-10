99b40861412d1538b3b91459b9890e9f1f99e136
Diamant <diamantkolshi@gmail.com>
2017-02-10 23:32:28 +0100

    add gamspec file and remove relative path

---
+++ alchemy_language.gemspec
@@ -0,0 +1,14 @@
+Gem::Specification.new do |s|
+  s.name          = 'alchemy_language'
+  s.version       = '1.0'
+  s.date          = '2017-02-10'
+  s.summary       = "Alchemy Language!"
+  s.description   = "AlchemyLanguage is a collection of APIs that offer text analysis through natural language processing."
+  s.authors       = ["Diamant Kolshi"]
+  s.email         = 'diamantkolshi@gmail.com'
+  s.files         =  Dir.glob("lib/**/*") + %w(README.md)
+  s.homepage      = 'https://github.com/diamantkolshi/alchemy_language'
+  s.license       = 'MIT'
+  s.require_path  = 'lib'
+  s.executables = `git ls-files -- bin/*`.split('\n').map{ |f| File.basename(f) }
+end 
--- lib/alchemy_language.rb
+++ lib/alchemy_language.rb
@@ -1,7 +1,7 @@
 require 'rest-client'
 require 'json'
 
-module AlchemyLanguage
+module AlchemyLanguages
   class << self
     def base_url_request
       "https://gateway-a.watsonplatform.net/calls"
@@ -13,16 +13,16 @@ module AlchemyLanguage
   end
 end
 
-require_relative "alchemy_language/active_method/base.rb"
-require_relative "alchemy_language/active_method/extra.rb"
-require_relative "alchemy_language/generator/alchemy_request.rb"
-require_relative "alchemy_language/active_model/base.rb"
-require_relative "alchemy_language/model/author.rb"
-require_relative "alchemy_language/model/concept.rb"
-require_relative "alchemy_language/model/date_extraction.rb"
-require_relative "alchemy_language/model/emotion_analysis.rb"
-require_relative "alchemy_language/model/targeted_emotion.rb"
-require_relative "alchemy_language/url_service.rb"
+require "alchemy_lang-uage/active_method/base.rb"
+require "alchemy_language/active_method/extra.rb"
+require "alchemy_language/generator/alchemy_request.rb"
+require "alchemy_language/active_model/base.rb"
+require "alchemy_language/model/author.rb"
+require "alchemy_language/model/concept.rb"
+require "alchemy_language/model/date_extraction.rb"
+require "alchemy_language/model/emotion_analysis.rb"
+require "alchemy_language/model/targeted_emotion.rb"
+require "alchemy_language/url_service.rb"
 
 # obj = AlchemyLanguage::UrlService.new("http://www.ibm.com/watson/")
 # puts obj.author.status
